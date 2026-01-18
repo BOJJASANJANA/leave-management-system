5a67ff0aba08e7dcff78ec49d80947374c92a3b9
Sanjana Bojja <bojjasanjana9@gmail.com>
2026-01-18 16:24:38 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,8 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="leaveuser",          # change if needed
-        password="leave123",  # change if needed
+        password="YOUR_PASSWORD",  # change if needed
         database="leave_db"
     )
+
 
